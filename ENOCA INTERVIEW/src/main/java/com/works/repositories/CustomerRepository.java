a53bfeba8f60218e52ef6c3d52edf674aa963353
BÜŞRA ÇERÇER <busra_cercer@outlook.com>
2024-03-01 15:07:55 +0300

    delete

--- ENOCA INTERVIEW/src/main/java/com/works/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.works.repositories;
-
-import com.works.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-
-}
